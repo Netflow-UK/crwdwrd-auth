b644ede4f10a86953cf00fbb5183d5b53ebe8eb0
Mark N Broadhead <MarkNBroadhead@gmail.com>
2022-07-28 02:23:24 -0600

    Change auth server to auth.dev.crwd.id

--- logout.php
+++ logout.php
@@ -6,16 +6,16 @@ use Jumbojett\OpenIDConnectClient;
 session_start();
 
 $oidc = new OpenIDConnectClient(
-  'http://localhost:4000',
-  'aaaaaaaa-9879-43e0-843b-141ea7b56ffb',
+  'https://auth.dev.crwd.id',
+  'cc7abc7a-9879-43e0-843b-141ea7b56ffb',
   null,
 );
 $oidc->setVerifyHost(false);
 $oidc->setVerifyPeer(false);
 $oidc->setHttpUpgradeInsecureRequests(false);
-$oidc->setHttpProxy("http://host.docker.internal:4000/");
+// $oidc->setHttpProxy("http://host.docker.internal:4000/");
 $oidc->setRedirectURL("http://localhost:3000");
-// $oidc->setCodeChallengeMethod('S256');
+$oidc->setCodeChallengeMethod('S256');
 $oidc->addRegistrationParam('post_logout_redirect_uris=["http://localhost:3000"]');
 
 session_destroy();
